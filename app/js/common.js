76bf20bd4b7fbc46058cc8998f68c19b72a4cdcb
zinovyevaleks <zinovyevaleks@gmail.com>
2019-01-23 17:23:57 +0700

    Правка файла JS

--- app/js/common.js
+++ app/js/common.js
@@ -1,43 +1,38 @@
 $(function() {
 
     //заявка на аудит
-    // $("#auditForm").submit(function() {
-    //     var th = $(this);
-    //     $.ajax({
-    //         type: "POST",
-    //         url: "https://tls.media/mail.php",
-    //         data: th.serialize()
-    //     }).done(function() {
-    //         dataLayer.push({
-    //             'event': 'formzayavka3'
-    //         });
-    //         alert("Благодарим вас за обращение. Наш мереджер свяжется с вами в ближайшее время.");
-    //         setTimeout(function() {
-    //             // Done Functions
-    //             th.trigger("reset");
-    //         }, 1000);
-    //     });
-    //     return false;
-	// });
+    $("#auditForm").submit(function() {
+        var th = $(this);
+        $.ajax({
+            type: "POST",
+            url: "https://tls.media/mail.php",
+            data: th.serialize()
+        }).done(function() {
+            alert("Благодарим вас за обращение. Наш мереджер свяжется с вами в ближайшее время.");
+            setTimeout(function() {
+                // Done Functions
+                th.trigger("reset");
+            }, 1000);
+        });
+        return false;
+	});
 
     //Подписка на рассылку
-    // $("#mailSubscription").submit(function() {
-    //     var th = $(this);
-    //     $.ajax({
-    //         type: "POST",
-    //         url: "https://tls.media/mail.php",
-    //         data: th.serialize()
-    //     }).done(function() {
-    //         dataLayer.push({
-    //             'event': 'formzayavka1'
-    //         });
-    //         alert("Благодарим вас за обращение. Наш мереджер свяжется с вами в ближайшее время.");
-    //         setTimeout(function() {
-    //             // Done Functions
-    //             th.trigger("reset");
-    //         }, 1000);
-    //     });
-    //     return false;
+    $("#mailSubscription").submit(function() {
+        var th = $(this);
+        $.ajax({
+            type: "POST",
+            url: "https://tls.media/mail.php",
+            data: th.serialize()
+        }).done(function() {
+            alert("Благодарим вас за обращение. Наш мереджер свяжется с вами в ближайшее время.");
+            setTimeout(function() {
+                // Done Functions
+                th.trigger("reset");
+            }, 1000);
+        });
+        return false;
+	});
 	// });
 
     $("form").submit(function() { //Change
