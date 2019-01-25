0a75ef56a06f1948f0dcb149898da680df5ef943
zinovyevaleks <zinovyevaleks@gmail.com>
2019-01-25 11:03:51 +0700

    event в отправку форм

--- app/js/common.js
+++ app/js/common.js
@@ -8,6 +8,9 @@ $(function() {
             url: "https://tls.media/mail.php",
             data: th.serialize()
         }).done(function() {
+            dataLayer.push({
+                'event': 'simmetriazayavka1'
+            });
             alert("Благодарим вас за обращение. Наш мереджер свяжется с вами в ближайшее время.");
             setTimeout(function() {
                 // Done Functions
@@ -25,6 +28,9 @@ $(function() {
             url: "https://tls.media/mail.php",
             data: th.serialize()
         }).done(function() {
+            dataLayer.push({
+                'event': 'simmetriazayavka2'
+            });
             alert("Благодарим вас за обращение. Наш мереджер свяжется с вами в ближайшее время.");
             setTimeout(function() {
                 // Done Functions
@@ -32,23 +38,6 @@ $(function() {
             }, 1000);
         });
         return false;
-	});
-	// });
-
-    $("form").submit(function() { //Change
-		var th = $(this);
-		$.ajax({
-			type: "POST",
-			url: "https://tls.media/mail.php", //Change
-			data: th.serialize()
-		}).done(function() {
-			alert("Thank you!");
-			setTimeout(function() {
-				// Done Functions
-				th.trigger("reset");
-			}, 1000);
-		});
-		return false;
 	});
 
 });
